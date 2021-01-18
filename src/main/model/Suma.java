f3390ae63e4ed71fc9779ed207c38c99df3bae36
alilopez <alzunun@upchiapas.edu.mx>
2021-01-18 11:39:31 -0600

    Add Exclusion Mutua

--- src/main/model/Suma.java
+++ src/main/model/Suma.java
@@ -1,14 +1,27 @@
 package main.model;
 
 import java.util.Observable;
+import java.util.concurrent.Semaphore;
 
 public class Suma extends Observable implements Runnable {
     public static int valor = 0;
+    private static Semaphore mutex = new Semaphore(1);
 
     @Override
     public void run() {
-        for (int i=1;i<=10018;i++)
-            valor++;
+
+            for (int i=1;i<=10018;i++) {
+                try {
+                    mutex.acquire();
+                    valor++;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    mutex.release();
+                }
+            }
+
         this.setChanged();
         this.notifyObservers(String.valueOf(valor));
     }
